92e16a999575cbe7bc3c240e0a3a93e7ba3d4e35
Kenrick Chan <kenrick.chan@outlook.com>
2026-02-05 12:33:47 -0500

    Add comments

--- sdet_geocode_api_test.ts
+++ sdet_geocode_api_test.ts
@@ -21,17 +21,24 @@ type responseObj = {
 };
 
 export default function () {
+  // Code to interact with the Google Geocode API
   let res = http.get(`${options.url}?address=${options.location}&key=${options.apiKey}`);
+  
+  // K6 check to determine that the request responded with a 200 result status
   check(res, { "status is 200": (res) => res.status === 200 });
+  
+  // Stringify -> Parse to access object data
   const resJsonString = JSON.stringify(res.json());
   const resJsonObj = JSON.parse(resJsonString);
   
+  // Store data from JSON object to an object
   const objData: responseObj= {
     city: resJsonObj.results[0].address_components[0].long_name,
     lat: resJsonObj.results[0].geometry.location.lat,
     lng: resJsonObj.results[0].geometry.location.lng
   };
 
+  // Print City, Lat, and Lng
   for (let key in objData) {
     console.log( `${key}: ${objData[key]}`)
   };
